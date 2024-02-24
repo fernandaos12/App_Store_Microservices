903093a222fae85b3553a46f7cf70e3d9b2760a9
fernanda.oliveira <fernandaos12@gmail.com>
2024-02-24 18:20:25 -0300

    Criando estrutura de microservico

---
+++ GeekShopping/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.Models
+{
+    public class ProductModel
+    {
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }             
+        public string Size { get; set; }
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
